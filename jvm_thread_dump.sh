d8ff11cc2054595b7a954469c80db00cda5864d8
Thilina Balasooriya <34156045+thilinaba@users.noreply.github.com>
2019-06-24 21:17:46 +0530

    Update jvm_thread_dump.sh

--- jvm_thread_dump.sh
+++ jvm_thread_dump.sh
@@ -13,7 +13,7 @@ DUMP_LOCATION="/tmp"
 for (( c=1; c<=$COUNT; c++ ))
 do
 echo "Taking thread dump $c ..."
-jstack -l $PID > $DUMP_LOCATION/$PID_thread_dump_$(date +"%Y-%m-%d_%T")_$c.txt
+jstack -l $PID > $DUMP_LOCATION/$PID-thread_dump_$(date +"%Y-%m-%d_%T")_$c.txt
 echo "Thread dump $c completed! Sleeping for $INTERVAL seconds"
 sleep $INTERVAL
 done
